5701e9bdc73b3da9cb0a72724dda078e5630ff08
Nauman Leghari <nauman.leghari@news.co.uk>
2017-10-20 12:51:50 +0100

    [added] common utils

---
+++ common_utils.py
@@ -0,0 +1,20 @@
+import random
+import string
+from pathlib import Path
+import shutil
+
+
+def create_dir(output_dir):
+    path = Path(output_dir)
+    if path.exists():
+        shutil.rmtree(output_dir)
+
+    path.mkdir()
+
+
+def random_string(length):
+    return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(length))
+
+
+def job_hash(job):
+    return random_string(len(job.get('q')))
