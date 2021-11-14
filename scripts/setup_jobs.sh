443abd2613b5eb06d0b415b10ee3d62d2e4a7939
namuan <575441+namuan@users.noreply.github.com>
2021-11-14 10:54:31 +0000

    Move requirements

--- scripts/setup_jobs.sh
+++ scripts/setup_jobs.sh
@@ -1,3 +1,3 @@
 cd bin-utils || exit
-#pip3 install -r requirements.txt --user
+#pip3 install -r requirements/base.txt --user
 bash ./scripts/start_screen.sh webpage-to-telegram 'python3 webpage_to_telegram.py'
