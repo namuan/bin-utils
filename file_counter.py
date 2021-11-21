8ea5d49b4429171550ebf8584633dd6825bb4399
namuan <575441+namuan@users.noreply.github.com>
2021-11-21 10:40:12 +0000

    Script to count number of files in a given folder matching a list of extensions

---
+++ file_counter.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+Count number of files by type in a directory
+
+input:
+    - directory
+    - file extensions
+
+output:
+    - total number of files
+"""
+from pathlib import Path
+from typing import List
+
+import fire
+
+from media_manager import valid_file
+
+
+def main(source_directory: str, extensions: List[str]):
+    print(f"Source directory: {source_directory}, extensions: {extensions}")
+    source_directory = Path(source_directory).expanduser()
+    if not source_directory.is_dir():
+        raise ValueError(f"{source_directory} is not a directory")
+
+    files_in_directory = list(
+        p.resolve()
+        for p in source_directory.glob("**/*")
+        if p.is_file() and p.suffix.lower() in extensions and valid_file(p)
+    )
+    print(files_in_directory[0])
+    print(f"Total files: {len(files_in_directory)}")
+
+
+if __name__ == "__main__":
+    fire.Fire(main)
