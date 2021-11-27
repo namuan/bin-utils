32b77c31c22b60074d75a46c1c9a4f512cfa753e
namuan <575441+namuan@users.noreply.github.com>
2021-11-27 09:06:09 +0000

    Extend demo webdriver script

--- firefox_browser_session.py
+++ firefox_browser_session.py
@@ -12,6 +12,10 @@ ln -s ~/Library/Application\ Support/Firefox/Profiles/65h6sl1r.default-release $
 """
 import fire
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 
 class FirefoxBrowserSession:
@@ -34,9 +38,22 @@ class FirefoxBrowserSession:
 session = FirefoxBrowserSession()
 
 
-def main(browser_profile: str):
+def main(browser_profile: str = None):
     session.start(browser_profile)
-    session.current().get("http://www.google.com")
+    current_session = session.current()
+    current_session.get("http://www.google.com")
+
+    # find the element and click it
+    current_session.find_element(By.XPATH, "//*[text()='I agree']").click()
+    query_input = current_session.find_element(By.NAME, "q")
+    query_input.send_keys("deskriders")
+    query_input.send_keys(Keys.RETURN)
+
+    # wait for an element to present
+    wait = WebDriverWait(current_session, 10)
+    wait.until(EC.visibility_of_element_located((By.XPATH, "//*[text()='https://www.deskriders.dev']")))
+
+    current_session.find_element(By.XPATH, "//*[text()='https://www.deskriders.dev']").click()
     session.stop()
 
 
