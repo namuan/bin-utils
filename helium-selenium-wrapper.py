d2c22676232ef0f626bbcc97d173cd6b09e950cd
namuan <575441+namuan@users.noreply.github.com>
2021-12-19 11:22:12 +0000

    Add an example to use `helium` a selenium API wrapper

---
+++ helium-selenium-wrapper.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+Demonstrates how to use helium to automate a web browser.
+"""
+import logging
+from argparse import ArgumentParser, RawDescriptionHelpFormatter
+
+from helium import start_firefox
+
+logging.basicConfig(
+    handlers=[
+        logging.StreamHandler(),
+    ],
+    format="%(asctime)s - %(filename)s:%(lineno)d - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    level=logging.INFO,
+)
+logging.captureWarnings(capture=True)
+
+
+def parse_args():
+    parser = ArgumentParser(
+        description=__doc__, formatter_class=RawDescriptionHelpFormatter
+    )
+    return parser.parse_args()
+
+
+def main(args):
+    print(args)
+    driver = start_firefox()
+    driver.execute_script("window.alert('Hello World')")
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    main(args)
