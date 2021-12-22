ca7ed579a408f3594b3aa3bcce5eba9490be8149
namuan <575441+namuan@users.noreply.github.com>
2021-12-22 07:34:17 +0000

    feat: Add Hugo blog post generation

--- hn_links.py
+++ hn_links.py
@@ -1,15 +1,27 @@
 #!/usr/bin/env python3
 """
 Grab links from HN Post and generate Markdown post with image thumbnails
-* To regenerate thumbnail, just delete the image file under thumbnails folder inside the post directory.
+It also creates a Hugo blog post from Markdown and images generated
 
-Usage: $ python hn-links.py -l https://news.ycombinator.com/item?id=25381191
+SUPPORT: To regenerate thumbnail, just delete the image file under thumbnails folder inside the post directory.
+SUPPORT: To remove any link from the blog post, delete the entry after the post is created **in the blog directory**
+Note down all the links somewhere then run the following command from blog directory to delete them
+E.g. Image links will be like
+
+![](/images/2021/12/21/httpsunixstackexchangecoma88682.png)
+![](/images/2021/12/21/httpscleaveapp.png)
+
+$ pbpaste | awk -F\/ '{print $6}' | tr -d ')' | while read img; do find . -name $img -delete; done # noqa: W605
+
+Usage:
+$ python hn-links.py -l https://news.ycombinator.com/item?id=25381191 -b <blog_directory> --open-in-editor
 """
 
 import logging
 import os
-import subprocess
-from argparse import ArgumentParser, RawDescriptionHelpFormatter
+from argparse import ArgumentParser
+from argparse import RawDescriptionHelpFormatter
+from datetime import datetime
 from pathlib import Path
 from urllib.parse import urlparse, parse_qs
 
@@ -19,7 +31,7 @@ from jinja2 import Environment, FileSystemLoader
 from requests import HTTPError
 from slug import slug
 
-from common.workflow import run_command, run_step, run_workflow
+from common.workflow import run_command, run_workflow
 
 UTF_ENCODING = "utf-8"
 
@@ -68,7 +80,15 @@ def html_parser_from(page_html):
     return BeautifulSoup(page_html, "html.parser")
 
 
-class CreateOutputFolder(object):
+def relative_image_directory():
+    now = datetime.now()
+    year = now.strftime("%Y")
+    month = now.strftime("%m")
+    day = now.strftime("%d")
+    return f"images/{year}/{month}/{day}"
+
+
+class CreateOutputFolder:
     """Create output folder using Post id in the temporary folder"""
 
     def run(self, context):
@@ -89,7 +109,7 @@ class CreateOutputFolder(object):
         context["thumbnails_folder"] = thumbnails_folder
 
 
-class GrabPostHtml(object):
+class GrabPostHtml:
     """Use requests to download HTML using a browser user agent"""
 
     def run(self, context):
@@ -102,7 +122,7 @@ class GrabPostHtml(object):
         context["page_html"] = page_html
 
 
-class ParsePostHtml(object):
+class ParsePostHtml:
     """Create BeautifulSoap parser from html"""
 
     def run(self, context):
@@ -110,7 +130,7 @@ class ParsePostHtml(object):
         context["bs"] = html_parser_from(page_html)
 
 
-class GrabPostTitle(object):
+class GrabPostTitle:
     """Extract page title using BeautifulSoap HTML parser"""
 
     def run(self, context):
@@ -118,7 +138,7 @@ class GrabPostTitle(object):
         context["hn_post_title"] = bs.title.string
 
 
-class ExtractAllLinksFromPost(object):
+class ExtractAllLinksFromPost:
     """Extract all links"""
 
     def run(self, context):
@@ -127,7 +147,7 @@ class ExtractAllLinksFromPost(object):
         context["all_links"] = all_links
 
 
-class KeepValidLinks(object):
+class KeepValidLinks:
     """Only keep interesting links"""
 
     def is_valid_link(self, link):
@@ -144,7 +164,7 @@ class KeepValidLinks(object):
         context["valid_links"] = valid_links
 
 
-class GrabChildLinkTitle(object):
+class GrabChildLinkTitle:
     """Get page title for each valid link"""
 
     def page_title_from(self, target_folder, link_in_comment):
@@ -171,7 +191,7 @@ class GrabChildLinkTitle(object):
         return page_title.strip()
 
 
-class GrabScreenThumbnail(object):
+class GrabScreenThumbnail:
     """For each link, get screen thumbnail"""
 
     def thumbnail(self, thumbnails_folder, page_link):
@@ -198,7 +218,7 @@ class GrabScreenThumbnail(object):
         context["links_with_metadata"] = links_with_metadata
 
 
-class GenerateMarkdown(object):
+class GenerateMarkdown:
     """Generate Markdown using the data in context"""
 
     def __init__(self):
@@ -219,7 +239,7 @@ class GenerateMarkdown(object):
         context["markdown_text"] = markdown_text
 
 
-class SaveMarkdown(object):
+class SaveMarkdown:
     """Save generated Markdown in a target document"""
 
     def run(self, context):
@@ -227,6 +247,106 @@ class SaveMarkdown(object):
         target_folder = context.get("target_folder")
         markdown_file_path = Path(target_folder) / "hn_links.md"
         markdown_file_path.write_text(markdown_text, encoding=UTF_ENCODING)
+
+
+class LoadMarkdownFile:
+    """Load file from HN Post directory"""
+
+    def run(self, context):
+        target_folder = context.get("target_folder")
+        markdown_file = Path(target_folder) / "hn_links.md"
+        md_content = markdown_file.read_text()
+        context["md_content"] = md_content
+
+
+class AddHugoHeader:
+    """Add blog header with metadata"""
+
+    def run(self, context):
+        md_content = context["md_content"]
+        post_date = datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
+        post_title = md_content.splitlines()[0].replace("#", "").strip()
+
+        post_header = f"""+++
+    date = {post_date}
+    title = "{post_title}"
+    description = ""
+    slug = ""
+    tags = ["hacker-news-links"]
+    categories = []
+    externalLink = ""
+    series = []
++++
+"""
+        post_with_header = (
+            post_header + os.linesep + os.linesep.join(md_content.splitlines()[2:])
+        )
+        context["post_with_header"] = post_with_header
+        post_file_name = slug(post_title) + ".md"
+        context["post_file_name"] = post_file_name
+
+
+class UpdateLinksInMarkdown:
+    """Use relative links in Markdown to point to images"""
+
+    def run(self, context):
+        post_with_header = context["post_with_header"]
+        target_folder = context.get("target_folder")
+        thumbnails_directory = Path(target_folder) / "thumbnails"
+        replace_from = f"![]({thumbnails_directory.as_posix()}"
+        replace_with = f"![](/{relative_image_directory()}"
+        md_with_updated_links = post_with_header.replace(replace_from, replace_with)
+        context["md_with_updated_links"] = md_with_updated_links
+
+
+class WriteBlogPost:
+    """Write to blog directory with correct file name"""
+
+    def run(self, context):
+        md_with_updated_links = context.get("md_with_updated_links")
+        blog_directory = context.get("blog_directory")
+        post_file_name = context["post_file_name"]
+        context["blog_page"] = "{}/content/posts/{}".format(
+            blog_directory, post_file_name
+        )
+        Path(context["blog_page"]).write_text(md_with_updated_links)
+        print("Created note at {}".format(context["blog_page"]))
+
+
+class CompressImages:
+    """Resize images and compress them"""
+
+    def run(self, context):
+        blog_directory = context.get("blog_directory")
+        target_folder = context.get("target_folder")
+        for img in Path(target_folder).glob("thumbnails/*"):
+            img_name = img.name
+            img_path = img.as_posix()
+            target_path = Path(
+                "{}/static/{}/{}".format(
+                    blog_directory, relative_image_directory(), img_name
+                )
+            )
+            if target_path.exists():
+                continue
+
+            Path(target_path).parent.mkdir(parents=True, exist_ok=True)
+            run_command(
+                f"convert {img_path} -resize 640x480 -quality 50% {target_path}"
+            )
+
+
+class OpenInEditor:
+    """Open blog post in editor defined by the environment variable EDITOR"""
+
+    def run(self, context):
+        open_in_editor = context["open_in_editor"]
+        if not open_in_editor:
+            return
+        blog_directory = context.get("blog_directory")
+        editor = os.environ.get("EDITOR")
+        print(f"Opening {blog_directory} in {editor}")
+        run_command(f"{editor} {blog_directory}")
 
 
 # Workflow definition
@@ -241,6 +361,12 @@ workflow_process = [
     GrabScreenThumbnail(),
     GenerateMarkdown(),
     SaveMarkdown(),
+    LoadMarkdownFile(),
+    AddHugoHeader(),
+    UpdateLinksInMarkdown(),
+    WriteBlogPost(),
+    CompressImages(),
+    OpenInEditor(),
 ]
 
 
@@ -256,6 +382,20 @@ def parse_args():
     )
     parser.add_argument(
         "-l", "--hn-link", required=True, type=str, help="Link to HN Post"
+    )
+    parser.add_argument(
+        "-b",
+        "--blog-directory",
+        type=str,
+        required=True,
+        help="Full path to blog directory",
+    )
+    parser.add_argument(
+        "-e",
+        "--open-in-editor",
+        action="store_true",
+        default=False,
+        help="Open blog site in editor",
     )
     parser.add_argument(
         "-v",
