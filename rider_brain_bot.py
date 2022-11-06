c1f1ff4b5d11019876c6d30d2aa63a368f573f1b
namuan <575441+namuan@users.noreply.github.com>
2022-11-06 09:45:29 +0000

    feat: Early handler framework and bookmark links in a database

--- rider_brain_bot.py
+++ rider_brain_bot.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 """
-Telegram bot to convert web page links to PDF
+Telegram bot to bookmark links
 """
 import argparse
 import logging
 import os
+from datetime import datetime
 from pathlib import Path
 
+import dataset
 import py_executable_checklist.workflow
 import telegram
 from dotenv import load_dotenv
@@ -18,14 +20,29 @@ from common_utils import fetch_html_page, html_parser_from, retry
 
 load_dotenv()
 
+logging.basicConfig(
+    handlers=[logging.StreamHandler()],
+    format="%(asctime)s - %(filename)s:%(lineno)d - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    level=logging.INFO,
+)
+logging.captureWarnings(capture=True)
+
+HOME_DIR = os.getenv("HOME")
+DB_FILE = "rider_brain.db"
+DB_CONNECTION_STRING = f"sqlite:///{HOME_DIR}/{DB_FILE}"
 OUTPUT_DIR = Path.home().joinpath("OutputDir", "web-to-pdf")
+BOOKMARKS_TABLE = "bookmarks"
+
+db = dataset.connect(DB_CONNECTION_STRING)
+logging.info(f"Connecting to database {DB_CONNECTION_STRING}")
+logging.info(f"Creating table {BOOKMARKS_TABLE}")
+bookmarks_table = db.create_table(BOOKMARKS_TABLE)
 
 
 def welcome(update: Update, _):
     if update.message:
-        update.message.reply_text(
-            "👋 Hi there. ⬇️ I'm a bot that converts web pages to PDFs. ⬆️. " "Try sending me a link to a web page"
-        )
+        update.message.reply_text("👋 Hi there. ⬇️ I'm a bot to save bookmarks ⬆️. " "Try sending me a link")
 
 
 def help_command(update: Update, _):
@@ -33,7 +50,7 @@ def help_command(update: Update, _):
         update.message.reply_text("Help!")
 
 
-def _handle_web_page(web_page_url: str) -> str:
+def handle_web_page(web_page_url: str) -> str:
     page_html = fetch_html_page(web_page_url)
     bs = html_parser_from(page_html)
     web_page_title = slug(bs.title.string if bs.title and bs.title.string else web_page_url)
@@ -44,12 +61,14 @@ def _handle_web_page(web_page_url: str) -> str:
 
 
 @retry(telegram.error.TimedOut, tries=3)
-def _update_user(bot, chat_id, original_message_id, reply_message_id, downloaded_file_path):
+def update_user(bot, chat_id, original_message_id, reply_message_id, incoming_text, downloaded_file_path):
     bot.delete_message(chat_id, original_message_id)
     bot.delete_message(chat_id, reply_message_id)
-    bot.send_message(chat_id, f"💾 Saved it here 👉 {downloaded_file_path}")
-    bot.send_chat_action(chat_id, "upload_document")
-    bot.sendDocument(chat_id, open(downloaded_file_path, "rb"))
+    if downloaded_file_path:
+        bot.send_chat_action(chat_id, "upload_document")
+        bot.sendDocument(chat_id, open(downloaded_file_path, "rb"))
+    else:
+        bot.send_message(chat_id, f"🔖 {incoming_text} bookmarked")
 
 
 def verified_chat_id(chat_id):
@@ -60,7 +79,37 @@ def verified_chat_id(chat_id):
     return True
 
 
-def _process_message(update: Update, context) -> None:
+class BaseHandler:
+    def __init__(self, url):
+        self.url = url
+
+    def bookmark(self) -> str:
+        file_location = self._bookmark()
+        entry_row = {
+            "source": self.__class__.__name__,
+            "link": self.url,
+            "created_at": datetime.now(),
+            "target_location": file_location,
+        }
+        bookmarks_table.insert(entry_row)
+        logging.info(f"Updated database: {entry_row}")
+        return file_location
+
+    def _bookmark(self) -> str:
+        pass
+
+
+class WebPage(BaseHandler):
+    def _bookmark(self) -> str:
+        logging.info(f"Bookmarking WebPage: {self.url}")
+        return handle_web_page(self.url)
+
+
+def message_handler_for(incoming_url) -> BaseHandler:
+    return WebPage(incoming_url)
+
+
+def process_message(update: Update, context) -> None:
     bot = context.bot
     chat_id = update.effective_chat.id
 
@@ -77,8 +126,11 @@ def _process_message(update: Update, context) -> None:
             f"Got {update_message_text}. 👀 at 🌎",
             disable_web_page_preview=True,
         )
-        downloaded_file_path = _handle_web_page(update_message_text)
-        _update_user(bot, chat_id, original_message_id, reply_message.message_id, downloaded_file_path)
+        message_handler = message_handler_for(update_message_text)
+        downloaded_file_path = message_handler.bookmark()
+        update_user(
+            bot, chat_id, original_message_id, reply_message.message_id, update_message_text, downloaded_file_path
+        )
         logging.info(f"✅ Document sent back to user {chat_id}")
     else:
         logging.warning(f"🚫 Ignoring message: {update_message_text}")
@@ -86,7 +138,7 @@ def _process_message(update: Update, context) -> None:
 
 def adapter(update: Update, context):
     try:
-        _process_message(update, context)
+        process_message(update, context)
     except Exception as e:
         logging.exception(e)
         update.message.reply_text(str(e))
@@ -112,18 +164,8 @@ def start_bot():
     return True
 
 
-def setup_logging():
-    logging.basicConfig(
-        handlers=[logging.StreamHandler()],
-        format="%(asctime)s - %(filename)s:%(lineno)d - %(message)s",
-        datefmt="%Y-%m-%d %H:%M:%S",
-        level=logging.INFO,
-    )
-    logging.captureWarnings(capture=True)
-
-
 def setup_directories():
-    OUTPUT_DIR.mkdir(exist_ok=True)
+    OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
 
 
 def parse_args():
@@ -133,6 +175,5 @@ def parse_args():
 
 if __name__ == "__main__":
     args = parse_args()
-    setup_logging()
     setup_directories()
     start_bot()
